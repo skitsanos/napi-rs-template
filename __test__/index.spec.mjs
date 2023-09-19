720c93e2b02a94f069546a0d822e0b0caf4a5a1f
skitsanos <info@skitsanos.com>
2023-09-19 12:09:08 +0300

    chore: dropped ava

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,9 @@
-import test from 'ava'
+import test from 'node:test';
+import assert from 'node:assert';
 
-import { sum } from '../index.js'
+import {sum} from '../index.js';
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+test('sum from native', () =>
+{
+    assert.strictEqual(sum(1, 2), 3, 'Must be 3');
+});
